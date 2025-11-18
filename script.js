ff3c8606b8077441cf0c3d9245e6d52f3db49283
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2025-11-18 17:56:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+//your JS code here
+const inputs = document.querySelectorAll(".code");
+
+inputs[0].focus();
+
+// Move forward when typing
+inputs.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    if (input.value.length === 1 && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  // Backspace behavior
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      input.value = "";            
+      
+      if (index > 0) {
+        inputs[index - 1].focus(); 
+      }
+    }
+  });
+});
